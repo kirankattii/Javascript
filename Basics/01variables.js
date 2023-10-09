401556451390f1e7d34c57cab32f9d5b6efb7339
Kiran Katti <139916130+kirankattii@users.noreply.github.com>
2023-10-09 13:18:59 +0000

    variables

---
+++ Basics/01variables.js
@@ -0,0 +1,12 @@
+console.log("kiran katti");
+const accId = "kiran123";
+
+let email = "kirankatti550@gmail.com"
+email = "kiran123456"
+console.log(email);
+console.table([accId,email])
+
+
+/*
+prefere not to use var because of issue in black scope
+*/
